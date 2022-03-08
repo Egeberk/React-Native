13043a499b5066be55236231d3d57327367e36b0
mert keskin <36448206+mrtksknn@users.noreply.github.com>
2022-03-08 11:01:21 +0300

    make changes for readability

--- Weather App/Components/ForecastData.js
+++ Weather App/Components/ForecastData.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { View, Text, ScrollView, SafeAreaView, Image,ImageBackground,StyleSheet } from 'react-native';
+import { 
+    View, 
+    Text, 
+    ScrollView, 
+    SafeAreaView, 
+    Image,
+    ImageBackground,
+    StyleSheet 
+} from 'react-native';
 
 const ForecastData = (props) => {
 
     return(
-        <View key={(props.item.dt).toString()} style={{justifyContent : 'center', alignItems: 'center', margin:5 }}>
-            <Text style={{color: 'white'}}>  {(props.index==0) ? 'Now':getDateHours(props.item.dt)}</Text>
-            <Image source={{
-                uri: 'https://openweathermap.org/img/wn/'+props.item.weather[0].icon+'@2x.png'}} style={{width: 30, height:30,marginLeft:4}}/>
-            <Text style={{color: 'white'}}>  {Math.round(props.item.temp)}°</Text>
+        <View 
+            key={(props.item.dt).toString()} 
+            style={{
+              justifyContent : 'center', 
+              alignItems: 'center', 
+              margin: 5 
+            }}
+        >
+            <Text style={{color: '#fff'}}>  
+                { (props.index==0) ? 'Now' : getDateHours(props.item.dt) }
+            </Text>
+            <Image 
+                source={{
+                    uri: 'https://openweathermap.org/img/wn/'+props.item.weather[0].icon+'@2x.png'
+                }} 
+                style={{
+                   width: 30, 
+                   height: 30,
+                   marginLeft: 4
+                }}
+            />
+            <Text style={{color: '#fff'}}>  
+                {Math.round(props.item.temp)}°
+            </Text>
         </View>
     )
 }
 
-function getDateHours(utcTime)
+function getDateHours(utcTime) // Bunu yukarı alabiliyorsan yukarı al daha iyi olur 
   {
     let time = ' ';
     var d = new Date(0); 
